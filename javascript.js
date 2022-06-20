db169afe1e2bd607c2583fb574c42011778deb28
Evan Kartheiser <ekarthei@gmail.com>
2022-06-20 11:44:11 -0700

    set up javascript to insert images from galleryImages array into the js-gallery div

--- javascript.js
+++ javascript.js
@@ -10,12 +10,20 @@ class galleryImage {
 
 let galleryImages = [
   {
-    url: "images\backlit-leaf-color_GalleryImg-1500px-long.jpg",
+    url: "images/backlit-leaf-color_GalleryImg-1500px-long.jpg",
     title: "Backlit Leaf",
     location: "Oakland, CA",
     caption:
       "This image was taken on a neighborhood walk during the pandemic, this period of my life encouraged me to slow down and enjoy the details",
     alt: "A backlit banana leaf with sunlight shining through.",
+  },
+
+  {
+    url: "images/hawaii-gold-coast_GalleryImg-1500px-long.jpg",
+    title: "Coast BW",
+    Location: "Kauai, HI",
+    caption: "Late afternoon on the Napali Coast",
+    alt: "Black and white image of the steep mountains on the nepali coast.",
   },
 ];
 
@@ -27,7 +35,7 @@ console.log(galleryImages[0]); // logs all key value pairs of the object
 console.log(galleryImages); // logs the list of objects and all their info
 
 const testImg = new galleryImage(
-  "imagescamelias-color_GalleryImg-1500px-long.jpg",
+  "images/camelias-color_GalleryImg-1500px-long.jpg",
   "Camelias",
   "Oakland, CA",
   "Caption for Camelias",
@@ -39,4 +47,14 @@ galleryImages.push(testImg); // appends testImg to the galleryImages list, using
 // for each object in galleryImages, log the title
 for (let i = 0; i < galleryImages.length; i++) {
   console.log(galleryImages[i].title);
+}
+
+// Using Javascript to add images to the .js-gallery div
+
+for (let i = 0; i < galleryImages.length; i++) {
+  let img = document.createElement("img");
+  img.src = galleryImages[i].url;
+
+  let block = document.getElementById("js-gallery");
+  block.appendChild(img);
 }
